fac0cd75deadf54f622b9846443f79cc15d4229f
Nabeel Tahir <93868996+nabeeltahirdeveloper@users.noreply.github.com>
2022-06-28 01:45:15 +0500

    bug fix

--- src/constants/ProjectCardConstant.js
+++ src/constants/ProjectCardConstant.js
@@ -1,6 +1,6 @@
-export const projectOneDesc = "I beleive to learn by doing projects. This project created by me when I am learning react basics. By designing this particular User-Interface I learn about the fundamentals of react like components, useState, useEffects and other common hooks. All the data displayed on the UI is not hard-coded but coming from an API call. I use TMDB (The Movie DataBase) API for this purpose. I enjoy building this project cause I have learn alot."
+export const projectOneDesc = "I believe to learn by doing projects. This project created by me when I am learning react basics. By designing this particular User-Interface I learn about the fundamentals of react like components, useState, useEffects and other common hooks. All the data displayed on the UI is not hard-coded but coming from an API call. I use TMDB (The Movie DataBase) API for this purpose. I enjoy building this project cause I have learn alot."
 
 
 
 
-export const projectTwoDesc = "This project is very close to my heart because this was my first project back in 2021 whwn I sarted learning react. Dy doing this project I have come to know the power of components of react. I just made a component and reuse it every where and in just somw minutes this project was completed. This is a fully respinsive UI of the well known tesla company."
+export const projectTwoDesc = "This project is very close to my heart because this was my first project back in 2021 when I sarted learning react. By doing this project I have come to know the power of components of react. I just made a component and reuse it every where and in just some minutes this project was completed. This is a fully responsive UI of the well known tesla company."
